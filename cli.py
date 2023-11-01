d58c9f7045376620037431e6797644feac8b18d8
“Rishi” <rishiraj.mullur1997@gmail.com>
2023-10-31 23:12:47 -0700

    Added logic for simple cli based moves

--- cli.py
+++ cli.py
@@ -3,17 +3,27 @@
 # For core game logic, see logic.py.
 
 from logic import make_empty_board
-
+from logic import get_winner
+from logic import show_current_board
+from logic import update_board
+from logic import other_player
 
 # Reminder to check all the tests
 
 if __name__ == '__main__':
     board = make_empty_board()
     winner = None
+    # Intiazlize the first move to X
+    char = 'X'
+
     while winner == None:
-        print("TODO: take a turn!")
-        # TODO: Show the board to the user.
-        # TODO: Input a move from the player.
-        # TODO: Update the board.
-        # TODO: Update who's turn it is.
-        winner = 'X'  # FIXME
+        print("Begin Turn!")
+        print("It's", char, "turn!")
+        show_current_board(board=board)
+        print("Enter the row and column of the board you want to add the move to:")
+        i = int(input("Enter the row:"))
+        j = int(input("Enter the column:"))
+        board = update_board(board=board,i=i,j=j,char=char)
+        char = other_player(player = char)
+        winner = get_winner(board)
+
--- logic.py
+++ logic.py
@@ -14,9 +14,58 @@ def make_empty_board():
 def get_winner(board):
     """Determines the winner of the given board.
     Returns 'X', 'O', or None."""
+    for i in board:
+        if i[0]==i[1]==i[2]=='X':
+            print('X won')
+            return 'X'
+        elif i[0]==i[1]==i[2]=='O':
+            print('O won')
+            return '0'
+
+    for j in range(3):
+    # print(board[0][j])
+        if board[0][j]==board[1][j]==board[2][j]=='X':
+            print('X won')
+            return 'X'
+        elif board[0][j]==board[1][j]==board[j]=='O':
+            print('O won')
+            return '0'
+
+    if board[0][0]==board[1][1]==board[2][2]=='X':
+        print('X won')
+        return 'X'
+    elif board[0][0]==board[1][1]==board[2][2]=='0':
+        print('0 won')
+        return '0'
     return None  # FIXME
 
 
 def other_player(player):
     """Given the character for a player, returns the other player."""
-    return "O"  # FIXME
+    if player == 'X':
+        return 'O'
+    else:
+        return 'X'
+
+
+def update_board(board,i,j,char):
+    i = i-1
+    j = j-1
+    if i >=3 or j >=3:
+        print("No such row or column")
+        return board
+    if board[i][j] == None:
+        board[i][j] = char
+        print("Check", board[i])
+        return board
+    else:
+        print("Illegal move!")
+        return board
+
+
+def show_current_board(board):
+    print(board[0])
+    print(board[1])
+    print(board[2])
+
+
